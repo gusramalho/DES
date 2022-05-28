405a87babe62194fa6b9d21259563754e2e0d992
gusramalho <gugarroliver@hotmail.com>
2022-05-28 10:57:09 -0300

    Init des

---
+++ src/des.c
@@ -0,0 +1,9 @@
+#include "des.h"
+
+int64_t encrypt(int64_t data) {
+  return 0;
+}
+
+int64_t decrypt(int64_t data) {
+  return 0;
+}
---
+++ src/des.h
@@ -0,0 +1,10 @@
+#ifndef DES_H
+#define DES_H
+
+#include "des_internals.h"
+
+int64_t encrypt(int64_t data);
+
+int64_t decrypt(int64_t data);
+
+#endif
